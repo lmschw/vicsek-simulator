df7430a7b156d1fc9c9cd07af407a504fa379fe5
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2024-02-16 11:24:26 +0100

    initial Vicsek simulator based on https://github.com/Stanvk/vicsek

---
+++ main/Animator.py
@@ -0,0 +1,63 @@
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import matplotlib.animation as animator
+
+class Animator(object):
+    
+    def prepareAnimation(self, matplotlibFigure, frames=100, frameInterval = 10):
+        """
+        Prepares the 2D animator object for animation.
+
+        parameters:
+        matplotlibFigure: Matplotlibs figure object.
+        frameInterval -- The interval between two frames.
+        frames -- The number of frames used in the animation.
+
+        returns self
+        """
+        self._matplotlibFigure = matplotlibFigure
+        self._frames = frames
+        self._frameInterval = frameInterval
+
+        return self
+
+    def setSimulationData(self, simulationData, domainSize):
+        """
+        Sets
+        keyword arguments:
+        simulationData -- The simulation data array.
+        domainSize -- The tuple that represents the lenghts of the square domain in each dimension.
+
+        return:
+        self
+        """        
+        self._time, self._positions, self._orientations = simulationData
+        self._domainSize = domainSize
+
+        return self
+
+
+    def showAnimation(self):
+        """
+        Shows the animation
+
+        returns self
+        """
+        self._getAnimation()
+        plt.show()
+
+        return self
+
+    def _getAnimation(self):
+        return self.animation if 'animation' in self.__dict__ else self._generateAnimation()
+
+    def _generateAnimation(self):
+        """
+        Generate the animation.
+        
+        returns
+        animation object
+        """
+        self.animation = FuncAnimation(self._matplotlibFigure, self._animate, interval=self._frameInterval, frames = self._frames)
+
+        return self.animation
