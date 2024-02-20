4fbaa5837129a8200a4d9da80bd4e184d36e3693
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2024-02-20 10:44:43 +0100

    add initial version of modes (excitement threshold)

---
+++ main/DisplayModes.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class DisplayModes(Enum):
+    NONE = 1,
+    EXAMPLE = 2,
+    EXCITEMENT = 3
--- main/kNearestWithModesVicsek2DExample.py
+++ main/kNearestWithModesVicsek2DExample.py
@@ -2,27 +2,28 @@
 Contains a simulation for the standard 2D Vicsek model
 """
 
-import VicsekWithKNeighbours
+import VicsekWithKNeighboursWithModes
 import MatplotlibAnimator
 import Animator2D
 
 import DefaultValues as dv
 
-n = 200
-k = 2
+n = 500
+k1 = 2
+k2 = 10
 noise = 0
-radius= 20
+radius= 100
 leavingAllowed = False
 
-simulator = VicsekWithKNeighbours.VicsekWithKNeighbours(domainSize=dv.DEFAULT_DOMAIN_SIZE_2D, numberOfParticles=n, k=k, noise=noise, radius=radius, particlesAllowedToLeave=leavingAllowed)
-simulationData = simulator.simulate()
+simulator = VicsekWithKNeighboursWithModes.VicsekWithKNeighboursWithModes(domainSize=dv.DEFAULT_DOMAIN_SIZE_2D, numberOfParticles=n, k1=k1, k2=k2, noise=noise, radius=radius, particlesAllowedToLeave=leavingAllowed)
+simulationData = simulator.simulate(tmax=3000)
 
 # Initalise the animator
 animator = MatplotlibAnimator.MatplotlibAnimator(simulationData, (100,100,100))
 
 # prepare the animator
 preparedAnimator = animator.prepare(Animator2D.Animator2D())
-preparedAnimator.setParameters(n=n, k=k, noise=noise, radius=radius, particlesAllowedToLeave=leavingAllowed)
+preparedAnimator.setParameters(n=n, k="k1={k1}, k2={k2}", noise=noise, radius=radius, particlesAllowedToLeave=leavingAllowed)
 
 # Display Animation
 preparedAnimator.showAnimation()
