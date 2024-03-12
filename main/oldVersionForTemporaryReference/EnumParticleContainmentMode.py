33a58dfb43d4e287acb2c7845feff37b44a8fb35
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2024-03-12 22:14:52 +0100

    delete temporary old files

--- main/oldVersionForTemporaryReference/EnumParticleContainmentMode.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class ParticleContainmentMode(str, Enum):
-    STANDARD = "STANDARD", # particles circle around once they cross the border of the domain
-    CONTAINED = "CONTAINED", # no leaving
-    LEAVING = "LEAVING" # upon crossing the border of the domain, the particles are lost
