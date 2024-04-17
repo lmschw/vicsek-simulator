1f0bbf72f407b828ee88781612ef59ae4f87890c
Lilly Schwarzenbach <lillyschwarzenbach@gmail.com>
2024-04-17 09:34:03 +0200

    add scatter plot & scatter plot video for neighbour (orientation difference vs. distance

---
+++ main/AnimatorScatter.py
@@ -0,0 +1,95 @@
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+import numpy as np
+
+import DefaultValues as dv
+
+class Animator(object):
+
+    def prepareAnimation(self, matplotlibFigure, ax, xlim=(0,10), ylim=(-1.1, 1.1), frames=100, frameInterval = 10):
+        """
+        Prepares the 2D animator object for animation.
+
+        parameters:
+        matplotlibFigure: Matplotlibs figure object.
+        frameInterval -- The interval between two frames.
+        frames -- The number of frames used in the animation.
+
+        returns self
+        """
+        self._matplotlibFigure = matplotlibFigure
+        self._ax = ax
+        self._xlim = xlim
+        self._ylim = ylim
+        self._frames = frames
+        self._frameInterval = frameInterval
+
+        return self
+
+    def setSimulationData(self, neighbourData):
+        """
+        Sets
+        keyword arguments:
+        simulationData -- The simulation data array.
+        domainSize -- The tuple that represents the lenghts of the square domain in each dimension.
+
+        return:
+        self
+        """        
+        self._time, self._positions = neighbourData
+
+        return self
+
+    def showAnimation(self):
+        """
+        Shows the animation
+
+        returns self
+        """
+        self._getAnimation()
+        plt.show()
+        
+        return self
+    
+    def saveAnimation(self, filename):
+        """
+        Saves the animation. Requires FFMPEG
+
+        returns
+        Animator
+        """
+        print("Saving commenced...")
+        animation = self._getAnimation()
+        animation.save(filename=filename, writer="ffmpeg")
+        print("Saving completed.")
+        return self
+    
+    def _getAnimation(self):
+        return self.animation if 'animation' in self.__dict__ else self._generateAnimation()
+
+    def _generateAnimation(self):
+        """
+        Generate the animation.
+        
+        returns
+        animation object
+        """
+        self.animation = FuncAnimation(self._matplotlibFigure, self._animate, interval=self._frameInterval, frames = self._frames)
+
+        return self.animation
+    
+    def _animate(self, i):
+        """
+        Animator class that goes through sim data.
+
+        keyword arguments:
+        i -- Loop index.
+        """
+
+        self._ax.clear()
+        self._ax.set_xlim(self._xlim)
+        self._ax.set_ylim(self._ylim)
+        posX = [el[0] for el in self._positions[i]]
+        posY = [el[1] for el in self._positions[i]]
+        scatter = self._ax.scatter(posX, posY)
+        return scatter
--- main/ServiceImages.py
+++ main/ServiceImages.py
@@ -3,12 +3,16 @@ import numpy as np
 from PIL import Image
 import pandas as pd
 import math
+import random
 
 import ServicePreparation
 import EvaluatorMultiAvgComp
 import ServiceSavedModel
 from EnumNeighbourSelectionMode import NeighbourSelectionMode
 from EnumMetrics import Metrics
+import ServiceMetric
+import ServiceGeneral
+import AnimatorScatter
 
 xOffsetsByNCol = {1: 0, 2: -0.5, 3: -0.8, 4: -1.5, 5: -2.2, 6: -3}
 
@@ -32,7 +36,7 @@ def createMultiPlotFromImages(title, nRows, nCols, rowLabels, colLabels, imgPath
     plt.tight_layout()
     plt.show()
 
-def createMultiPlotFromScratch(xLabels, yLabels, data, index, title=None, xAxisLabel=None, yAxisLabel=None, savePath=None, xlim =(0,1000), ylim=None):
+def createMultiPlotFromScratch(xLabels, yLabels, data, index, title=None, xAxisLabel=None, yAxisLabel=None, savePath=None, xlim =(0,1000), ylim=None, legendRows=1):
     fontsize = 8
     nRows = len(yLabels)
     nCols = len(xLabels)
@@ -49,8 +53,11 @@ def createMultiPlotFromScratch(xLabels, yLabels, data, index, title=None, xAxisL
                 df.plot(ax=axes[x][y], legend=False)
 
     if nRows == 1:
-        axes[0].set_title(xLabels[0], fontsize=fontsize)
-        axes[0].set_xlim(xlim)
+        #axes[0].set_title(xLabels[0], fontsize=fontsize)
+        #axes[0].set_xlim(xlim)
+        for ax, col in zip(axes, xLabels):
+            ax.set_title(col, fontsize=fontsize)
+            ax.set_xlim(xlim)
 
         for ax, row in zip(axes, yLabels):
             ax.set_ylabel(row, fontsize=fontsize)
@@ -68,7 +75,7 @@ def createMultiPlotFromScratch(xLabels, yLabels, data, index, title=None, xAxisL
         
     fig.subplots_adjust(bottom=0.3, wspace=0.33)
     xOffset = xOffsetsByNCol.get(nCols)
-    plt.legend(loc='upper center', bbox_to_anchor=(xOffset, -0.3),fancybox=False, shadow=False, ncol=len(index))
+    plt.legend(loc='upper center', bbox_to_anchor=(xOffset, -0.3),fancybox=False, shadow=False, ncol=len(index)/legendRows)
     if xAxisLabel != None:
         fig.supxlabel(xAxisLabel, va="bottom")
         #plt.xlabel(xLabel)
@@ -79,3 +86,100 @@ def createMultiPlotFromScratch(xLabels, yLabels, data, index, title=None, xAxisL
     if savePath != None:
         plt.savefig(savePath)
     plt.show()
+
+# SCATTERPLOT OF NEIGHBOURS
+def createNeighbourScatterplot(positions, orientations, numberOfExampleParticles, index, title=None, selectRandomly=True, numberOfSteps=10, steps=None, radius=10, ylim=(-1.1,1.1), savePath=None):
+    if selectRandomly == True:
+        selectedIndices = random.sample(range(len(positions[0])), numberOfExampleParticles)
+    else:
+        selectedIndices = range(numberOfExampleParticles)
+    
+    if steps == None:
+        steps = range(0, len(positions), int(len(positions)/numberOfSteps))
+
+    plotData = {}
+    print(f"selectedIndices={selectedIndices}, steps={steps}")
+    for i in selectedIndices:
+        ServiceGeneral.logWithTime(f"Evaluating particle {i}")
+        pointsForI = []
+        for timestep in steps:
+            neighbours = ServiceMetric.findNeighbours(i, positions[timestep], radius)
+            pointsForTimestep = []
+            for neighbourIdx in neighbours:
+                orientationDifference = ServiceMetric.cosAngle(orientations[timestep][i], orientations[timestep][neighbourIdx])
+                distance = math.sqrt((positions[timestep][i][0]-positions[timestep][neighbourIdx][0])**2 + (positions[timestep][i][1]-positions[timestep][neighbourIdx][1])**2)
+                pointsForTimestep.append([distance, orientationDifference])
+            pointsForI.append(pointsForTimestep)
+        plotData[i] = pointsForI
+
+    fontsize = 8
+
+    ServiceGeneral.logWithTime(f"Creating the plots...")
+    nRows = numberOfExampleParticles
+    nCols = len(steps)
+    fig, axes = plt.subplots(nrows=nRows, ncols=nCols, sharex=True, sharey=True)
+
+    if title != None:
+        fig.suptitle(title)
+    for x, partIdx in enumerate(selectedIndices):
+        for y in range(len(steps)):
+            df = pd.DataFrame(plotData.get(partIdx)[y], columns=index)  
+            if nRows == 1:
+                df.plot.scatter(ax=axes[y], legend=False)
+            else:
+                df.plot.scatter(x=index[0], y=index[1], ax=axes[x][y], legend=False)
+
+    ServiceGeneral.logWithTime("Created the subplots. Adding decorations...")
+    if nRows == 1:
+        #axes[0].set_title(xLabels[0], fontsize=fontsize)
+        #axes[0].set_xlim(xlim)
+        for ax, col in zip(axes, steps):
+            ax.set_title(col, fontsize=fontsize)
+
+        for ax, row in zip(axes, selectedIndices):
+            ax.set_ylabel(row, fontsize=fontsize)
+            ax.set_ylim(ylim)
+    else:
+        for ax, col in zip(axes[0, :], steps):
+            ax.set_title(col, fontsize=fontsize)
+            #ax.set_xlim((0,5000))
+        for ax, row in zip(axes[:, 0], selectedIndices):
+            ax.set_ylabel(row, fontsize=fontsize)
+            ax.set_ylim(ylim)
+    fig.subplots_adjust(bottom=0.3, wspace=0.33)
+    xOffset = xOffsetsByNCol.get(nCols)
+    #plt.legend(loc='upper center', bbox_to_anchor=(xOffset, -0.3),fancybox=False, shadow=False, ncol=len(index))
+    fig.supxlabel(index[0], va="bottom")
+    fig.supylabel(index[1])
+    plt.tight_layout()
+    if savePath != None:
+        plt.savefig(savePath)
+    plt.show()
+
+def createNeighbourScatterplotVideo(positions, orientations,  radius=10, ylim=(-1.1,1.1), savePath=None):
+    i = 1
+    pointsForI = []
+    times = []
+    for timestep in range(len(positions)):
+        neighbours = ServiceMetric.findNeighbours(i, positions[timestep], radius)
+        times.append(timestep)
+        pointsForTimestep = []
+        for neighbourIdx in neighbours:
+            orientationDifference = ServiceMetric.cosAngle(orientations[timestep][i], orientations[timestep][neighbourIdx])
+            distance = math.sqrt((positions[timestep][i][0]-positions[timestep][neighbourIdx][0])**2 + (positions[timestep][i][1]-positions[timestep][neighbourIdx][1])**2)
+            pointsForTimestep.append([distance, orientationDifference])
+        pointsForI.append(pointsForTimestep)
+
+    nRows = 1
+    nCols = 1
+    fig, axes = plt.subplots(nrows=nRows, ncols=nCols, sharex=True, sharey=True)
+
+    # Initalise the animator
+    animator = AnimatorScatter.Animator()
+
+    # prepare the animator
+    preparedAnimator =  animator.prepareAnimation(fig, axes, xlim=(0,radius), ylim=ylim, frames=len(times))
+    preparedAnimator.setSimulationData(neighbourData=(times, pointsForI))
+
+    # Display Animation
+    preparedAnimator.showAnimation()
