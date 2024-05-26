04f74574a98350056e6f2d900e5a6a7dfb043bc5
lmschw <lillyschwarzenbach@gmail.com>
2024-05-26 14:12:23 +0200

    add other comparison heatmap methods

--- main/exampleDensityVsRadiusPlot.py
+++ main/exampleDensityVsRadiusPlot.py
@@ -20,15 +20,37 @@ for thresholdType in [ThresholdType.TWO_THRESHOLDS, ThresholdType.TWO_THRESHOLDS
             type = "maxorder"
             startValue = 1
 
-        for i in range(1, 4):
+        for i in range(1, 3):
             startI = time.time()
             for threshold in thresholdVals:
                 startTh = time.time()
                 ServiceGeneral.logWithTime(f"Started i={i}, threshold={threshold}")
                 savePath = f"radius-vs-density-{initialState}-{type}-{thresholdType.value}-threshold={threshold}-i={i}.svg"
-                ServiceImages.createDensityVsRadiusPlot(type=type, threshold=threshold, radiusVals=radiusVals, densityVals=densityVals, initialState=initialState, startValue=startValue, i=i, savePath=savePath)
+                ServiceImages.createDensityVsRadiusPlot(type=type, thresholdType=thresholdType, threshold=threshold, radiusVals=radiusVals, densityVals=densityVals, initialState=initialState, startValue=startValue, i=i, savePath=savePath)
                 endTh = time.time()
                 ServiceGeneral.logWithTime(f"Completed i={i}, threshold={threshold} in {ServiceGeneral.formatTime(endTh-startTh)}")
+            endI = time.time()
+            ServiceGeneral.logWithTime(f"Completed radius-vs-density i={i} in {ServiceGeneral.formatTime(endI-startI)}")
+
+            startI = time.time()
+            for radius in radiusVals:
+                startTh = time.time()
+                ServiceGeneral.logWithTime(f"Started i={i}, threshold={threshold}")
+                savePath = f"threshold-vs-density-{initialState}-{type}-{thresholdType.value}-radius={radius}-i={i}.svg"
+                ServiceImages.createDensityVsThresholdPlot(type=type, thresholdType=thresholdType, radius=radius, thresholdVals=thresholdVals, densityVals=densityVals, initialState=initialState, startValue=startValue, i=i, savePath=savePath)
+                endTh = time.time()
+                ServiceGeneral.logWithTime(f"Completed threshold-vs-density i={i}, threshold={threshold} in {ServiceGeneral.formatTime(endTh-startTh)}")
+            endI = time.time()
+            ServiceGeneral.logWithTime(f"Completed i={i} in {ServiceGeneral.formatTime(endI-startI)}")
+
+            startI = time.time()
+            for density in densityVals:
+                startTh = time.time()
+                ServiceGeneral.logWithTime(f"Started i={i}, threshold={threshold}")
+                savePath = f"threshold-vs-radius-{initialState}-{type}-{thresholdType.value}-radius={radius}-i={i}.svg"
+                ServiceImages.createThresholdVsRadiusPlot(type=type, thresholdType=thresholdType, density=density, thresholdVals=thresholdVals, radiusVals=radiusVals, initialState=initialState, startValue=startValue, i=i, savePath=savePath)
+                endTh = time.time()
+                ServiceGeneral.logWithTime(f"Completed threshold-vs-radius  i={i}, threshold={threshold} in {ServiceGeneral.formatTime(endTh-startTh)}")
             endI = time.time()
             ServiceGeneral.logWithTime(f"Completed i={i} in {ServiceGeneral.formatTime(endI-startI)}")
 
