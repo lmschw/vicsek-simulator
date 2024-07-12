3042d9c0c25fe397c1bc194d4469f7cc82cfaf95
lmschw <lillyschwarzenbach@gmail.com>
2024-07-12 12:25:36 +0200

    update densityrun

--- main/exampleDensityRun_1-7-24.py
+++ main/exampleDensityRun_1-7-24.py
@@ -28,7 +28,8 @@ def getOrderDisorderValue(switchType):
         case SwitchType.K:
             return 5, 1
         case SwitchType.NEIGHBOUR_SELECTION_MODE:
-            return NeighbourSelectionMode.FARTHEST, NeighbourSelectionMode.NEAREST
+            #return NeighbourSelectionMode.FARTHEST, NeighbourSelectionMode.NEAREST
+            return NeighbourSelectionMode.FARTHEST, NeighbourSelectionMode.LEAST_ORIENTATION_DIFFERENCE
 
 speed = 1
 
@@ -57,7 +58,7 @@ densities = [0.01, 0.05, 0.09]
 psteps = 100
 numbersOfPreviousSteps = [psteps]
 durations = [1000]
-ks = [1,5]
+ks = [1, 5]
 radii = [5, 10, 20] # area is always 4x bigger than the last
 
 neighbourSelectionModes = [NeighbourSelectionMode.ALL,
@@ -103,6 +104,7 @@ for density in densities:
         print(f"d={density}, n={n}, r={radius}, size={domainSize}")
 
         
+        """
         # ---neighbourSelectionMode only (3000) for all 6 modes
         tmax = tmaxWithoutEvent
 
@@ -143,7 +145,7 @@ for density in densities:
         saveLocation = f"{baseLocation}/local"
         areas = [(domainSize[0]/2, domainSize[1]/2, radius)]
         tmax = tmaxWithEvent
-
+        """
         for duration in durations:
             # --- single event, no switchvals for all modes with k = 1 and k = 5 (15000)        
             for k in ks:
@@ -199,4 +201,138 @@ for density in densities:
 
                                 endRun = time.time()
                                 ServiceGeneral.logWithTime(f"Completed d={density}, r={radius}, drn={duration}, k={k}, nsm={neighbourSelectionMode.name}, {initialStateString}, eventEffect={eventEffectOrder.name}, i={i} in {ServiceGeneral.formatTime(endRun-startRun)}")
+        """
+        saveLocation = f"{saveLocation}/switchingActive"
+
+        for orderValue in [NeighbourSelectionMode.ALL, NeighbourSelectionMode.RANDOM, NeighbourSelectionMode.HIGHEST_ORIENTATION_DIFFERENCE]:
+            for disorderValue in disorderNeighbourSelectionModes: 
+                startNsm = time.time()
+                for duration in durations:
+                    switchType = SwitchType.NEIGHBOUR_SELECTION_MODE
+                    orderValue, disorderValue = getOrderDisorderValue(switchType)      
+                    for k in ks:
+                        for initialStateString in ["ordered", "random"]:
+                            if initialStateString == "ordered":
+                                startValue = orderValue
+                            else:
+                                startValue = disorderValue
+
+                            for eventEffectOrder in eventEffects:
+                                for i in range(iStart,iStop):
+                                    ServiceGeneral.logWithTime(f"Started nsm switch d={density}, r={radius}, drn={duration}, k={k}, {initialStateString}, eventEffect={eventEffectOrder.name}, i={i}")
+                                    event1 = ExternalStimulusOrientationChangeEventDuration(
+                                                    startTimestep=e1Start,
+                                                    endTimestep=e1Start + duration,
+                                                    percentage=percentage,
+                                                    angle=angle,
+                                                    eventEffect=eventEffectOrder,
+                                                    movementPattern=movementPattern,
+                                                    movementSpeed=1,
+                                                    perceptionRadius=radius,
+                                                    distributionType=distributionType,
+                                                    areas=areas,
+                                                    )
+                                    
+                                    events = [event1]
+
+                                    startRun = time.time()
+
+                                    if initialStateString == "ordered":
+                                        initialState = ServicePreparation.createOrderedInitialDistributionEquidistancedIndividual(startValue, domainSize, n)
+                                    simulator = VicsekWithNeighbourSelectionSwitchingCellBasedIndividualsDuration.VicsekWithNeighbourSelection(
+                                                                                                    neighbourSelectionModel=startValue, 
+                                                                                                    domainSize=domainSize, 
+                                                                                                    numberOfParticles=n, 
+                                                                                                    k=k, 
+                                                                                                    noise=noise, 
+                                                                                                    radius=radius,
+                                                                                                    switchType=switchType,
+                                                                                                    switchValues=(orderValue, disorderValue),
+                                                                                                    thresholdType=thresholdType,
+                                                                                                    orderThresholds=threshold,
+                                                                                                    numberPreviousStepsForThreshold=psteps,
+                                                                                                    switchBlockedAfterEventTimesteps=blockSteps,
+                                                                                                    speed=speed,
+                                                                                                    )
+                                    if initialStateString == "ordered":
+                                        simulationData, colours, switchValues = simulator.simulate(tmax=tmax, initialState=initialState, events=events)
+                                    else:
+                                        simulationData, colours, switchValues = simulator.simulate(tmax=tmax, events=events)
+
+                                    # Save model values for future use
+                                    eventsString = f"{event1.timestep}-{event1.eventEffect.val}"
+                                    savePath = f"{saveLocation}/local_1e_switchType={switchType.value}_{initialStateString}_st={startValue.value}_o={orderValue.value}_do={disorderValue.value}_d={density}_n={n}_r={radius}_k={k}_noise={noisePercentage}_drn={duration}_{eventsString}_{i}"
+                                    ServiceSavedModel.saveModel(simulationData=simulationData, colours=colours, switchValues=switchValues, path=f"{savePath}.json", modelParams=simulator.getParameterSummary())
+
+                                    endRun = time.time()
+                                    ServiceGeneral.logWithTime(f"Completed nsm-switch d={density}, r={radius}, drn={duration}, k={k}, {initialStateString}, eventEffect={eventEffectOrder.name}, i={i} in {ServiceGeneral.formatTime(endRun-startRun)}")
+        endNsm = time.time()
+        ServiceGeneral.logWithTime(f"Completed nsm-switch in {ServiceGeneral.formatTime(endNsm-startNsm)}")
+        """
+        startK = time.time()
+        for duration in durations:
+            switchType = SwitchType.K
+            orderValue, disorderValue = getOrderDisorderValue(switchType)      
+            for neighbourSelectionMode in neighbourSelectionModes:
+                for initialStateString in ["ordered", "random"]:
+                    if initialStateString == "ordered":
+                        startValue = orderValue
+                    else:
+                        startValue = disorderValue
+
+                    for eventEffectOrder in eventEffects:
+                        for i in range(iStart,iStop):
+                            ServiceGeneral.logWithTime(f"Started k-switch d={density}, r={radius}, drn={duration}, nsm={neighbourSelectionMode.name}, {initialStateString}, eventEffect={eventEffectOrder.name}, i={i}")
+                            event1 = ExternalStimulusOrientationChangeEventDuration(
+                                            startTimestep=e1Start,
+                                            endTimestep=e1Start + duration,
+                                            percentage=percentage,
+                                            angle=angle,
+                                            eventEffect=eventEffectOrder,
+                                            movementPattern=movementPattern,
+                                            movementSpeed=1,
+                                            perceptionRadius=radius,
+                                            distributionType=distributionType,
+                                            areas=areas,
+                                            )
+                            
+                            events = [event1]
+
+                            startRun = time.time()
+
+                            if initialStateString == "ordered":
+                                initialState = ServicePreparation.createOrderedInitialDistributionEquidistancedIndividual(startValue, domainSize, n)
+                            simulator = VicsekWithNeighbourSelectionSwitchingCellBasedIndividualsDuration.VicsekWithNeighbourSelection(
+                                                                                            neighbourSelectionModel=neighbourSelectionMode, 
+                                                                                            domainSize=domainSize, 
+                                                                                            numberOfParticles=n, 
+                                                                                            k=startValue, 
+                                                                                            noise=noise, 
+                                                                                            radius=radius,
+                                                                                            switchType=switchType,
+                                                                                            switchValues=(orderValue, disorderValue),
+                                                                                            thresholdType=thresholdType,
+                                                                                            orderThresholds=threshold,
+                                                                                            numberPreviousStepsForThreshold=psteps,
+                                                                                            switchBlockedAfterEventTimesteps=blockSteps,
+                                                                                            speed=speed,
+                                                                                            )
+                            if initialStateString == "ordered":
+                                simulationData, colours, switchValues = simulator.simulate(tmax=tmax, initialState=initialState, events=events)
+                            else:
+                                simulationData, colours, switchValues = simulator.simulate(tmax=tmax, events=events)
+
+                            # Save model values for future use
+                            eventsString = f"{event1.timestep}-{event1.eventEffect.val}"
+                            savePath = f"{saveLocation}/local_1e_switchType={switchType.value}_{initialStateString}_st={startValue}_o={orderValue}_do={disorderValue}_d={density}_n={n}_r={radius}_nsm={neighbourSelectionMode.value}_noise={noisePercentage}_drn={duration}_{eventsString}_{i}"
+                            ServiceSavedModel.saveModel(simulationData=simulationData, colours=colours, switchValues=switchValues, path=f"{savePath}.json", modelParams=simulator.getParameterSummary())
+
+                            endRun = time.time()
+                            ServiceGeneral.logWithTime(f"Completed k-switch d={density}, r={radius}, drn={duration}, nsm={neighbourSelectionMode.name}, {initialStateString}, eventEffect={eventEffectOrder.name}, i={i} in {ServiceGeneral.formatTime(endRun-startRun)}")
+
+                            endK = time.time()
+ServiceGeneral.logWithTime(f"Completed k-switch in {ServiceGeneral.formatTime(endK-startK)}")
 """
+endK = time.time()
+ServiceGeneral.logWithTime(f"Completed nsm-switch in {ServiceGeneral.formatTime(endNsm-startNsm)}")
+ServiceGeneral.logWithTime(f"Completed nsm-switch + k-switch in {ServiceGeneral.formatTime(endK-startNsm)}")
