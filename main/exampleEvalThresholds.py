9728a5eae6c874611af5657bea8c71a1a16b1deb
lmschw <lillyschwarzenbach@gmail.com>
2025-07-24 20:40:23 +0200

    add eval script for thresholds

---
+++ main/exampleEvalThresholds.py
@@ -0,0 +1,220 @@
+import time
+import numpy as np
+
+from EnumNeighbourSelectionMode import NeighbourSelectionMode
+from EnumMetrics import Metrics
+from EnumSwitchType import SwitchType
+from EnumDistributionType import DistributionType
+from EnumEventEffect import EventEffect
+from EnumThresholdType import ThresholdType
+from EnumMovementPattern import MovementPattern
+
+import EvaluatorMultiAvgComp
+import ServiceSavedModel
+import ServicePreparation
+import ServiceGeneral
+import ServiceImages
+import ServiceMetric
+
+import DefaultValues as dv
+import AnimatorMatplotlib
+import Animator2D
+
+
+def eval(density, n, radius, eventEffect, metric, type, switch_threshold, nsm=None, k=None, combo=None, evalInterval=1, tmax=15000):
+    xlim = (0, tmax)
+    threshold = 0.01
+    if metric in [Metrics.ORDER, Metrics.DUAL_OVERLAY_ORDER_AND_PERCENTAGE]:
+        ylim = (0, 1.1)
+    elif metric == Metrics.CLUSTER_NUMBER_WITH_RADIUS:
+        ylim = (0, n)
+        threshold = 0.995
+    else:
+        ylim = (0, 50)
+   
+    yAxisLabel = metric.label
+    startEval = time.time()
+    if type == "global":
+        ServiceGeneral.logWithTime(f"d={density}, r={radius}, nsm={nsm}, k={k}, metric={metric.name}, type={type}") 
+    else:
+        ServiceGeneral.logWithTime(f"d={density}, r={radius}, nsm={nsm}, k={k}, combo={combo}, eventEffect={eventEffect.val}, metric={metric.name}, type={type}")
+    modelParams = []
+    simulationData = []
+    colours = []
+    switchTypes = []
+
+    for initialStateString in ["ordered", "random"]:
+        if type in ["nsmsw", "ksw"]:
+            orderValue, disorderValue = combo
+        if type == "nsmsw": 
+            if initialStateString == "ordered":
+                nsm = orderValue
+            else:
+                nsm = disorderValue
+        elif type == "ksw": 
+            if initialStateString == "ordered":
+                k = orderValue
+            else:
+                k = disorderValue
+        
+        if type == "nsmsw":
+            baseFilename = f"{baseDataLocation}{levelDataLocation}local_nsmsw_1ev_{initialStateString}_st={nsm.value}_d={density}_n={n}_r={radius}_nsmCombo={disorderValue.value}-{orderValue.value}_k={k}_noise={noisePercentage}_speed={speed}_ee={eventEffect.val}_threshold={switch_threshold}"
+        elif type == "ksw":
+            baseFilename = f"{baseDataLocation}{levelDataLocation}local_ksw_1ev_{initialStateString}_st={k}_d={density}_n={n}_r={radius}_nsm={nsm.value}_kCombo={disorderValue}-{orderValue}_ee={eventEffect.val}_noise={noisePercentage}_speed={speed}_threshold={switch_threshold}"
+
+        filenames = ServiceGeneral.createListOfFilenamesForI(baseFilename=baseFilename, minI=iStart, maxI=iStop, fileTypeString="json")
+        if type not in ["nosw", "global"]:
+            modelParamsDensity, simulationDataDensity, coloursDensity, switchTypeValues = ServiceSavedModel.loadModels(filenames, loadSwitchValues=True)
+            switchTypes.append(switchTypeValues)
+        else:
+            modelParamsDensity, simulationDataDensity = ServiceSavedModel.loadModels(filenames, loadSwitchValues=False, fromCsv=False)
+        modelParams.append(modelParamsDensity)
+        simulationData.append(simulationDataDensity)
+
+#paths.append(f"density-vs-noise_ORDER_mode-comparision_n={n}_k=1_radius=10_density={density}_noise={noisePercentage}%_hierarchical_clustering_threshold=0.01.png")
+#createMultiPlotFromImages(title, numX, numY, rowLabels, colLabels, paths)
+    threshold = 0.01
+    evaluator = EvaluatorMultiAvgComp.EvaluatorMultiAvgComp(modelParams, metric, simulationData, evaluationTimestepInterval=evalInterval, threshold=threshold, switchTypeValues=switchTypes, switchTypeOptions=combo)
+    
+    labels = ["ordered", "disordered"]
+    if metric == Metrics.DUAL_OVERLAY_ORDER_AND_PERCENTAGE:
+        labels = ["ordered - order", "ordered - percentage of order-inducing value", "disordered - order", "disordered - percentage of order-inducing value"]
+    if type == "nosw":
+        savePath = f"{saveLocation}{metric.val}_d={density}_n={n}_r={radius}_nosw_nsm={nsm.value}_k={k}_ee={eventEffect.val}_th={switch_threshold}.svg"
+    elif type == "nsmsw":
+        savePath = f"{saveLocation}{metric.val}_d={density}_n={n}_r={radius}_swt=MODE_o={orderValue.value}_do={disorderValue.value}_k={k}_ee={eventEffect.val}_th={switch_threshold}.svg"
+    elif type == "ksw":
+        savePath = f"{saveLocation}{metric.val}_d={density}_n={n}_r={radius}_swt=K_o={orderValue}_do={disorderValue}_nsm={nsm.value}_ee={eventEffect.val}_th={switch_threshold}.svg"
+    elif type == "global":
+        savePath = f"{saveLocation}{metric.val}_d={density}_n={n}_r={radius}_global_nsm={nsm.value}_k={k}_th={switch_threshold}.svg"
+
+    evaluator.evaluateAndVisualize(labels=labels, xLabel=xAxisLabel, yLabel=yAxisLabel, colourBackgroundForTimesteps=[e1Start, e1Start+duration], showVariance=True, xlim=xlim, ylim=ylim, savePath=savePath)    
+    endEval = time.time()
+    print(f"Duration eval {ServiceGeneral.formatTime(endEval-startEval)}") 
+
+
+def getLabelsFromNoisePercentages(noisePercentages):
+    return [f"{noisePercentage}% noise" for noisePercentage in noisePercentages]
+
+def getLabelsFromKValues(ks):
+    return [f"k={k}" for k in ks]
+
+def getLabelsFromNeighbourSelectionModes(neighbourSelectionModes):
+    return [neighbourSelectionMode.name for neighbourSelectionMode in neighbourSelectionModes]
+
+def getLabelsFromEventEffects(eventEffects):
+    return [eventEffect.label for eventEffect in eventEffects]
+
+xLabel = "time steps"
+
+speed = 1
+
+angle = np.pi
+blockSteps = -1
+thresholdType = ThresholdType.HYSTERESIS
+threshold = [0.1]
+
+
+#domainSize = ServicePreparation.getDomainSizeForConstantDensity(0.09, 100)
+domainSize = (50, 50)
+
+distTypeString = "lssmid"
+distributionType = DistributionType.LOCAL_SINGLE_SITE
+percentage = 100
+movementPattern = MovementPattern.STATIC
+e1Start = 5000
+e2Start = 10000
+e3Start = 15000
+
+noisePercentages = [1,4] # to run again with other noise percentages, make sure to comment out anything that has fixed noise (esp. local)
+densities = [0.01]
+psteps = 100
+numbersOfPreviousSteps = [psteps]
+durations = [1000]
+ks = [1,5]
+
+neighbourSelectionModes = [
+                           NeighbourSelectionMode.NEAREST,
+                           NeighbourSelectionMode.FARTHEST,
+                           NeighbourSelectionMode.LEAST_ORIENTATION_DIFFERENCE,
+                           NeighbourSelectionMode.HIGHEST_ORIENTATION_DIFFERENCE,
+                           NeighbourSelectionMode.ALL,
+                           NeighbourSelectionMode.RANDOM
+                           ]
+
+orderNeighbourSelectionModes = [NeighbourSelectionMode.ALL,
+                                NeighbourSelectionMode.RANDOM,
+                                NeighbourSelectionMode.FARTHEST,
+                                NeighbourSelectionMode.HIGHEST_ORIENTATION_DIFFERENCE]
+
+disorderNeighbourSelectionModes = [NeighbourSelectionMode.NEAREST,
+                                   NeighbourSelectionMode.LEAST_ORIENTATION_DIFFERENCE]
+
+localNeighbourSelectionmodes = [NeighbourSelectionMode.LEAST_ORIENTATION_DIFFERENCE,
+                                NeighbourSelectionMode.HIGHEST_ORIENTATION_DIFFERENCE]
+
+eventEffects = [EventEffect.ALIGN_TO_FIXED_ANGLE,
+                EventEffect.AWAY_FROM_ORIGIN,
+                EventEffect.RANDOM]
+
+eventEffectsOrder = [
+                     EventEffect.ALIGN_TO_FIXED_ANGLE,
+                     ]
+
+eventEffectsDisorder = [EventEffect.AWAY_FROM_ORIGIN,
+                        EventEffect.RANDOM]
+
+saveLocation = f"results_240725/"
+iStart = 1
+iStop = 11
+
+baseDataLocation = "J:/threshold_investigation/"
+
+densities = [0.09]
+radii = [10]
+interval = 1
+kMax = 5
+noisePercentage = 1
+
+# ------------------------------------------------ LOCAL ---------------------------------------------------------------
+levelDataLocation = ""
+
+data = {}
+
+ks = [1, 5]
+
+# K VS. START
+metrics = [
+           Metrics.ORDER
+           ]
+xAxisLabel = "timesteps"
+
+
+startTime = time.time()
+
+duration = 1000
+tmax = 15000
+
+thresholds = [0.1, 0.2, 0.3, 0.4, 0.5]
+
+for density in densities:
+    n = int(ServicePreparation.getNumberOfParticlesForConstantDensity(density, domainSize))
+    for radius in radii:
+        for threshold in thresholds:
+            threshold = [threshold]
+            for nsmCombo in [[NeighbourSelectionMode.FARTHEST, NeighbourSelectionMode.NEAREST]]:
+                for k in [1]:
+                    for eventEffect in eventEffects:
+                        for metric in metrics:
+                            eval(density=density, n=n, radius=radius, switch_threshold=threshold, eventEffect=eventEffect, metric=metric, type="nsmsw", k=k, combo=nsmCombo, evalInterval=interval, tmax=tmax)
+            
+            for nsm in [NeighbourSelectionMode.NEAREST, NeighbourSelectionMode.LEAST_ORIENTATION_DIFFERENCE]:
+                for kCombo in [[5,1]]:
+                    for eventEffect in eventEffects:
+                        for metric in metrics:
+                            eval(density=density, n=n, radius=radius, switch_threshold=threshold, eventEffect=eventEffect, metric=metric, type="ksw", nsm=nsm, combo=kCombo, evalInterval=interval, tmax=tmax)
+
+endTime = time.time()
+print(f"Total duration: {ServiceGeneral.formatTime(endTime-startTime)}")
+    
+
